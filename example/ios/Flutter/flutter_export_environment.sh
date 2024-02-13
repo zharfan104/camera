984779f78ebc6dbd4dcf6b62e3f38557bd053d80
Zharfan Akbar <zharfan.akbar104@gmail.com>
2024-02-13 08:04:28 +0700

    Change mockito and run flutter pub get

---
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/lpt-799/fvm/versions/3.7.7"
+export "FLUTTER_APPLICATION_PATH=/Users/lpt-799/Code/custom-package/camera-0.10.5+5/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.dart_tool/package_config.json"
